97bcf1d347f85d12923aa863f8ac684130fbfdb3
Henry Li <henry1943@163.com>
2017-01-12 17:57:26 +0800

    Remove console

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  console.log(req.user);
   res.render('templates/index', { title: 'Welcome' });
 });
 

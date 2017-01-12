7710c08747b9de43b0b3304840aa6271e3b81f11
Henry Li <henry1943@163.com>
2017-01-12 18:02:40 +0800

    Display user info

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('templates/index', { title: 'Welcome' });
+  res.render('templates/index', { title: 'Welcome', user: req.user });
 });
 
 export default router;

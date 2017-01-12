8a17e39d327e7eedd36dcab05145894ce19e29d1
Henry Li <henry1943@163.com>
2017-01-12 23:25:25 +0800

    Add api

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,6 +5,7 @@ import app from '../app';
 const router = Router();
 
 router.use('/', require('./user').default);
+router.use('/api', require('../api').default);
 
 router.get('/', (req, res) => {
   res.render('index', { title: 'Welcome', app, user: req.user });

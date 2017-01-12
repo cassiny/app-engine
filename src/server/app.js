32e03f04ae6996d7d09d741430d21038e3ba33e6
Henry Li <henry1943@163.com>
2017-01-12 10:56:40 +0800

    Add app.js

---
+++ src/server/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.get('/', (req, res) => {
+  res.type('html');
+  res.send('<h1>Welcome</h1><p>Welcome to Cassiny AppEngine</p>');
+});
+
+export default app;

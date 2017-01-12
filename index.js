a0f84ad3a26cf1cd8f3882b9a0b8043e78d2003a
Henry Li <henry1943@163.com>
2017-01-12 11:07:15 +0800

    Add starting mode info

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 var env = process.env.NODE_ENV;
 if (env === 'production')
 {
+  console.info('Cassiny AppEngine server is now starting in [PRODUCTION] mode...');
   require('./dist/server');
 }
 else
 {
+  console.info('Cassiny AppEngine server is now starting in [DEVELOPMENT] mode...');
   require('./src/server');
 }

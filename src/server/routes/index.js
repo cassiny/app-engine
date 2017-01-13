810c8e2109a1df8d1e034afb2bde680208a6be3f
Henry Li <henry1943@163.com>
2017-01-13 15:30:54 +0800

    Rename index to welcome

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,7 +8,7 @@ router.use('/', require('./user').default);
 router.use('/api', require('../api').default);
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome', app, req });
+  res.render('welcome', { title: 'Welcome', app, req });
 });
 
 export default router;

10b307a5cad4003ca8c3490a6ce70fc8427819f9
Henry Li <henry1943@163.com>
2017-01-13 15:37:55 +0800

    Add spa

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,7 +8,11 @@ router.use('/', require('./user').default);
 router.use('/api', require('../api').default);
 
 router.get('/', (req, res) => {
-  res.render('welcome', { title: 'Welcome', app, req });
+  if (!req.user) {
+    res.render('welcome', { title: 'Welcome', app, req });
+  } else {
+    res.render('spa', { title: 'AppEngine', app, req });
+  }
 });
 
 export default router;

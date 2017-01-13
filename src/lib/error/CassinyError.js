6110602d7f60532fbf2522eaf1413c5a5b4e1862
Ziseon Jiao <ziseon.jiao@sap.com>
2017-01-13 16:03:17 +0800

    Add error specification

--- src/lib/error/CassinyError.js
+++ src/lib/error/CassinyError.js
@@ -6,14 +6,19 @@ class CassiniyError extends Error {
     this.code = code;
   }
 
-};
+}
 
 [
-  'USERNAME_ALREADY_EXIST',
-  'EMAIL_ALREADY_EXIST',
-].forEach((name, index) => {
-  Object.defineProperty(exports, name, {
-    get: () => new CassiniyError(index, name),
+  // server side - internal errors.
+  { type: 'UNHANDLE_ERROR', code: 0 },
+
+  // server side - client visible errors.
+  { type: 'USERNAME_ALREADY_EXIST', code: 1001 },
+  { type: 'EMAIL_ALREADY_EXIST', code: 1002 },
+
+].forEach((errorType) => {
+  Object.defineProperty(exports, errorType.type, {
+    get: () => new CassiniyError(errorType.code, errorType.type),
     configurable: false,
   });
 });

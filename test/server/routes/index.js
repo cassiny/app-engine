9688831765a7dbdc7ab6ca4402f5a314e69c6c01
emengjzs <emengjzs@live.com>
2017-01-14 00:24:10 +0800

    Use functions instead of lambda

--- test/server/routes/index.js
+++ test/server/routes/index.js
@@ -6,10 +6,9 @@ const root = 'http://localhost:3000';
 // but it conflicts with Airbnb Guide Style
 // see https://mochajs.org/#arrow-functions
 describe('Home page', () => {
-  const self = this;
-  it('should be ok.', (done) => {
+  it('should be ok.', function it(done) {
     // wait until server is ready
-    self.timeout(20000);
+    this.timeout(20000);
     http.get(root, (res) => {
       res.statusCode.should.be.equal(200);
       done();

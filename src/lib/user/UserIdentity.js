41d3671911e3bd97a0a81878db7efdf4290d8b21
Henry Li <henry1943@163.com>
2017-01-13 09:23:00 +0800

    Use JSON.stringify as serialize, while JSON.parse as deserialize

--- src/lib/user/UserIdentity.js
+++ src/lib/user/UserIdentity.js
@@ -1,43 +1,17 @@
-import UserManager from './UserManager';
-
 export default class UserIdentity {
   constructor({ id, username, nickname, email }) {
-    this._id = id;
-    this._email = email;
-    this._username = username;
-    this._nickname = nickname;
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  get email() {
-    return this._email;
-  }
-
-  get username() {
-    return this._username;
-  }
-
-  get nickname() {
-    return this._nickname;
+    this.id = id;
+    this.email = email;
+    this.username = username;
+    this.nickname = nickname;
   }
 
   serialize() {
-    return this.id;
+    return JSON.stringify(this);
   }
 
-  static async deserialize(id) {
-    const user = await UserIdentity.findById(id);
+  static async deserialize(json) {
+    const user = JSON.parse(json);
     return user;
-  }
-
-  static async findById(id) {
-    const user = await UserManager.getUserById(id);
-    if (user) {
-      return new UserIdentity(user);
-    }
-    return null;
   }
 }

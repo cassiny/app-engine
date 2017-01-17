ebdd719ab888ea62188cff7056fcfcd7277b6eaf
Henry Li <henry1943@163.com>
2017-01-17 20:43:22 +0800

    Remove commas

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     join: ['./user/join/index.js', './user/join/res/index.less'],
     welcome: ['./welcome/res/index.less'],
 
-    'project-detail-app': ['./project/app/ProjectDetailApp.jsx', './project/res/index.less'],
+    'project-detail-app': ['./project/app/ProjectDetailApp.jsx', './project/res/index.less']
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
@@ -21,52 +21,52 @@ module.exports = {
       bootstrap: path.resolve('./node_modules/bootstrap'),
       joi: 'joi-browser',
       common: path.resolve('./src/client/common'),
-      'variables.less': path.resolve('./src/client/common/res/variables.less'),
-    },
+      'variables.less': path.resolve('./src/client/common/res/variables.less')
+    }
   },
   output: {
     path: path.resolve('./dist/assets'),
     publicPath: '/',
-    filename: './js/[name].min.js',
+    filename: './js/[name].min.js'
   },
   devServer: {
-    contentBase: path.resolve('../dist/assets'),
+    contentBase: path.resolve('../dist/assets')
   },
   module: {
     loaders: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loaders: ['babel-loader'],
+        loaders: ['babel-loader']
       },
       {
         test: /\.css/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
       },
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader'),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
       },
       {
         test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=10240&name=images/[name].[hash].[ext]&outputPath=/images/',
-      },
-    ],
+        loader: 'url-loader?limit=10240&name=images/[name].[hash].[ext]&outputPath=/images/'
+      }
+    ]
   },
   plugins: [
     new ExtractTextPlugin('./css/[name].min.css'),
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery',
+      jQuery: 'jquery'
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       filename: './js/vendor.min.js',
-      minChunks: Infinity,
-    }),
-  ],
+      minChunks: Infinity
+    })
+  ]
 };

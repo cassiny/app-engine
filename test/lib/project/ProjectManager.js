5770d77339e2c32b9443239a3d5ae8aa74a50412
Ziseon Jiao <ziseon.jiao@sap.com>
2017-01-17 15:30:21 +0800

    Add ProjectManager

---
+++ test/lib/project/ProjectManager.js
@@ -0,0 +1,25 @@
+import ProjectManager from './../../../src/lib/project/ProjectMaganer';
+import User from './../../../src/lib/user/model/User';
+
+
+
+describe('Project Manager', () => {
+  let userId;
+  before((done) => {
+    User.findOne({ username: 'admin' }).exec((err, user) => {
+      userId = user.id;
+      done();
+    });
+  });
+
+
+  it('should return project list of owner admin', async () => {
+    const projects = await ProjectManager.getProjectsByUserId(userId);
+    projects.should.have.size(1);
+  });
+
+  it('should return example project of owner admin', async () => {
+    const project = await ProjectManager.getProjectByPath(userId, 'example');
+    project.should.have.property('name').which.is.equal('example');
+  });
+});

b0c8411dc3f705d287e240e7ad8f98566604f19f
Henry Li <henry1943@163.com>
2017-01-17 13:40:48 +0800

    Add project route

---
+++ src/server/routes/project.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+import app from '../app';
+
+const router = Router();
+
+router.use('/:username/:path', (req, res, next) => {
+  if (req.user.username !== req.params.username) {
+    res.status(403).end();
+    return;
+  }
+  // TODO: Check if the project is existed, otherwise throw 404
+  next();
+});
+
+router.get('/:username/:path/*', (req, res) => {
+  res.render('project/project-detail-app', { title: '', app, req });
+});
+
+export default router;

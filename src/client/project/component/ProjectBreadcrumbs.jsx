b2d909b0c874c3f523be23cf61f598cc11126130
LucasYuNju <LucasYuNju@gmail.com>
2017-01-17 14:59:42 +0800

    Update breadcrumb

--- src/client/project/component/ProjectBreadcrumbs.jsx
+++ src/client/project/component/ProjectBreadcrumbs.jsx
@@ -2,9 +2,17 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
 export default class ProjectBreadcrumbs extends Component {
+  static contextTypes: {
+    router: React.PropTypes.func.isRequired,
+  };
+
   static propTypes = {
     username: PropTypes.string.isRequired,
-    path: PropTypes.string.isRequired,
+    path: PropTypes.string,
+  };
+
+  static defaultProps = {
+    path: '',
   };
 
   render() {
@@ -12,10 +20,10 @@ export default class ProjectBreadcrumbs extends Component {
     return (
       <ol className="breadcrumb">
         <li className="breadcrumb-item">
-          <Link to="#">{username}</Link>
+          <Link to={`/project/${username}/`}>{username}</Link>
         </li>
         <li className="breadcrumb-item">
-          <Link to="#">{path}</Link>
+          <Link to={`/project/${username}/${path}/`}>{path}</Link>
         </li>
       </ol>
     );

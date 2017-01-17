be2e34c6e8736a54f09703e94d0756b93936766a
emengjzs <emengjzs@live.com>
2017-01-17 22:48:00 +0800

    Return handler base on ENV

--- src/lib/error/internalErrorHandler.js
+++ src/lib/error/internalErrorHandler.js
@@ -1,6 +1,11 @@
 export default function internalErrorHandler() {
+  if (process.env.NODE_ENV === 'production') {
+    return (err, req, res, next) => {
+      res.status(500).json({ message: 'System is busy' });
+      // next(err);
+    };
+  }
   return (err, req, res, next) => {
-    res.status(500).json({ message: 'System is busy' });
-    next(err);
+    res.status(500).send(err);
   };
 }

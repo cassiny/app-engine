39d5b06a47d1094198620d0b0e30ed98d74d2b45
Ziseon Jiao <ziseon.jiao@sap.com>
2017-01-17 15:30:51 +0800

    Add project route

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,5 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.use('/user', require('./user').default);
+router.use('/project', require('./project').default);
 
 export default router;

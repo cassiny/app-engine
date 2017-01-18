89cea3d88ffb67da79dbce9dba11bd108d165959
Ziseon Jiao <ziseon.jiao@sap.com>
2017-01-18 17:59:24 +0800

    Add POST project

--- src/server/routes/project.js
+++ src/server/routes/project.js
@@ -6,7 +6,7 @@ import app from '../app';
 
 const router = Router();
 
-router.use('/:username', (req, res, next) => {
+router.param('username', (req, res, next) => {
   if (req.user.username !== req.params.username) {
     res.status(403).end();
   } else {
@@ -14,7 +14,7 @@ router.use('/:username', (req, res, next) => {
   }
 });
 
-router.use('/:username/:path', async (req, res, next) => {
+const projectPathValidationFilter = async (req, res, next) => {
   const project = await ProjectManager.getProjectByPath(req.user.id, req.params.path);
   if (project) {
     res.set('projectJSON', JSON.stringify(project));
@@ -22,7 +22,7 @@ router.use('/:username/:path', async (req, res, next) => {
   } else {
     res.status(404).end();
   }
-});
+};
 
 
 router.get('/:username', (req, res) => {
@@ -32,7 +32,36 @@ router.get('/:username', (req, res) => {
   });
 });
 
-router.get('/:username/:path*', (req, res) => {
+router.post('/:username/:path', async (req, res, next) => {
+  const projectInput = {
+    userId: req.user.id,
+    name: req.body.name,
+    path: req.params.path,
+    desc: req.body.desc,
+  };
+
+  const repositoryInput = {
+    url: req.body['git.url'],
+    username: req.body['git.username'],
+    password: req.body['git.password'],
+    branch: req.body['git.branch'],
+  };
+
+  try {
+    const project = await ProjectManager.createProject(projectInput, repositoryInput);
+    if (project) {
+      res.redirect(`/project/${req.user.username}/${projectInput.path}`);
+    }
+  } catch (err) {
+    if (err.isClientError) {
+      res.redirect(`/project/${req.user.username}`);
+    } else {
+      next(err);
+    }
+  }
+});
+
+router.get('/:username/:path*', projectPathValidationFilter, (req, res) => {
   res.render('project/project-detail-app',
     {
       title: 'Loading',

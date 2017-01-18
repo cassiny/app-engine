5d99c5c0c0cb9082780c2ffd891b052a56c750ae
Ziseon Jiao <ziseon.jiao@sap.com>
2017-01-18 14:34:43 +0800

    Test: update hook in test

--- test/server/api/project/index.js
+++ test/server/api/project/index.js
@@ -1,29 +1,55 @@
-import mongoose from 'mongoose';
-
 import agent from './../../agent';
 import logger from './../../../../src/lib/log/logger';
+import Project from './../../../../src/lib/project/model/Project';
 
 const username = 'admin';
 const projectPath = 'example';
 
-const adminLogin = (done) => {
+const all = (...handlers) => {
+  const len = handlers.length;
+
+  let i = -1;
+  const next = () => {
+    i += 1;
+    try {
+      if (i < len - 1) {
+        handlers[i](next);
+      } else if (i === len - 1) {
+        handlers[i]();
+      }
+    } catch (err) {
+      logger.error(err);
+    }
+  };
+
+  try {
+    next();
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+
+
+const adminLogin = next =>
   agent
       .post('/login')
       .send({
         password: 'admin',
         login_name: 'Admin',
       })
-      .end(done);
-};
+      .end(next)
+;
 
 describe('POST /api/project', () => {
-  before(adminLogin);
-  before((done) => {
-    mongoose.connection.db.dropCollection('project', (err) => {
-      logger.debug(err);
-      done();
-    });
-  });
+  before(done => all(
+    adminLogin,
+    next =>
+      Project.findOneAndRemove({ id: projectId, path: projectPath }).exec((err, res) => next()),
+    done
+  ));
+
+  let projectId;
 
   it('should create project successful', (done) => {
     agent.post(`/api/project/${username}/${projectPath}`)
@@ -39,6 +65,7 @@ describe('POST /api/project', () => {
     .expect(200)
     .expect((res) => {
       res.body.shoud.have.property('id').which.should.be.number();
+      projectId = res.body.id;
     })
     .end(agent.debug(done));
   });

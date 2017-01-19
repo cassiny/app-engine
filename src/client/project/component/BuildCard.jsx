a1defa64aa1d6518ad57d02beb556bd8525dc5a1
CompileYouth <CompileYouth@163.com>
2017-01-19 14:35:27 +0800

    Add build-card.less.

---
+++ src/client/project/component/BuildCard.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class BuildCard extends Component {
+  render() {
+    return (<div className="build-card passed">
+      <div className="two-line col1">
+        <div className="row-item build-num">#5</div>
+        <div className="row-item commit-info">
+          <div className="branch">master</div>
+          <div className="comment">Add README.md.</div>
+        </div>
+      </div>
+      <div className="two-line col2">
+        <div className="row-item state">passed</div>
+        <div className="row-item hash">asdfasdf</div>
+      </div>
+      <div className="two-line col3">
+        <div className="row-item duration">54s</div>
+        <div className="row-item calendar">4min</div>
+      </div>
+    </div>);
+  }
+}

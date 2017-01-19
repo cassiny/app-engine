a02e1f4c0e18a00333d74aae6f0e72f05e26363a
CompileYouth <CompileYouth@163.com>
2017-01-19 16:17:36 +0800

    Modify BuildHistoryTabPage.

--- src/client/project/tab/BuildHistoryTabPage.jsx
+++ src/client/project/tab/BuildHistoryTabPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 
 import BuildCard from '../component/BuildCard';
 
-export default class BuildHistoryTabPage extends React.Component {
+export default class BuildHistoryTabPage extends Component {
+  static propTypes = {
+    buildHistory: PropTypes.arrayOf(PropTypes.shape({
+      revision: PropTypes.number
+    })).isRequired
+  };
+
+  static defaultProps = {
+    buildHistory: []
+  };
+
   render() {
-    return (<BuildCard />);
+    const { buildHistory } = this.props;
+    return (<div className="buildHistory">
+      {buildHistory.map(this.renderBuild)}
+    </div>);
+  }
+
+  renderBuild = (build) => {
+    return (<BuildCard key={build.revision} build={build} />);
   }
 }

92060950fdadb698fa001e748455aa4560102274
lilongllong <2453085960@qq.com>
2017-01-19 13:16:06 +0800

    Add Card component.

---
+++ src/client/project/component/Card.jsx
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Card extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    extra: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    style: PropTypes.object
+  };
+
+  static defaultProps = {
+    className: '',
+    title: '',
+    extra: null,
+    children: null,
+    style: {}
+  };
+
+  render() {
+    return (<div className={`app-engine-card panel panel-default ${this.props.className}`} style={this.props.style}>
+      <div className="card-header panel-heading">
+        <div className="card-title panel-title"><h3>{this.props.title}</h3></div>
+        <div className="card-extra">{this.props.extra}</div>
+      </div>
+      <div className="card-body panel-body">{this.props.children}</div>
+    </div>);
+  }
+}

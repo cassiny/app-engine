ef6d4ceff5f1fa929c238ce2c3dff7e020c66a7d
LucasYuNju <LucasYuNju@gmail.com>
2017-01-19 17:31:20 +0800

    Update css

--- src/client/project/component/InstanceList.jsx
+++ src/client/project/component/InstanceList.jsx
@@ -31,13 +31,13 @@ export default class InstanceList extends Component {
     const className = `instance-list-item col-md-9 offset-md-1 ${stateMap[instance.state]}`;
     return (
       <div className={className} key={instance.name}>
-        <div className="info-group col-md-6">
-          <a className="name h4">{instance.name}</a>
+        <div className="info-group col-md-5">
+          <a className="name">{instance.name}</a>
           <a className="address">{`${instance.host}:${instance.port}`}</a>
         </div>
         <span className="state col-md-4">{stateMap[instance.state]}</span>
-        <span className="build col-md-2">{`build#${instance.build.revision}`}</span>
-        <button type="button" className="btn btn-default col-md-1">Default</button>
+        <span className="build col-md-3">{`build#${instance.build.revision}`}</span>
+        <button type="button" className="btn btn-default col-md-1">Start</button>
       </div>
     );
   }
